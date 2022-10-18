a219229305b6683eeb9467df701ac8b0733ec194
Kyle Yuan Yang Lee <56005940+k297lee@users.noreply.github.com>
2022-10-18 17:57:26 -0400

    Time: 269 ms (69.32%), Space: 17.8 MB (30.44%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,25 +1,42 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+        
         combinations = defaultdict(list)
         for i in range(len(wordList)):
             for j in range(len(beginWord)):
                 combinations[wordList[i][:j] + "*" + wordList[i][j + 1:]].append(i)
         
-        q = deque()
-        q.append((beginWord, 1))
+        q_begin = deque()
+        q_end = deque()
+        q_begin.append(beginWord)
+        q_end.append(endWord)
         
-        seen = set()
-        while q:
-            word, length = q.popleft();
-            
-            for i in range(len(beginWord)):
-                genericWord = word[:i] + "*" + word[i + 1:]
-                for j in combinations[genericWord]:
-                    if wordList[j] == endWord:
-                        return length + 1
-                    if wordList[j] not in seen:
-                        seen.add(wordList[j])
-                        q.append((wordList[j], length + 1))
-                combinations[genericWord] = []
+        seen_1 = {beginWord: 1}
+        seen_2 = {endWord: 1}
+        
+        def bfs(q, seen_begin, seen_end):
+            q_size = len(q)
+            for _ in range(q_size):
+                word = q.popleft()
+                for i in range(len(beginWord)):
+                    genericWord = word[:i] + "*" + word[i + 1:]
+                    for j in combinations[genericWord]:
+                        if wordList[j] in seen_end:
+                            return seen_begin[word] + seen_end[wordList[j]]
+                        if wordList[j] not in seen_begin:
+                            seen_begin[wordList[j]] = seen_begin[word] + 1
+                            q.append(wordList[j])
+            return 0
+        
+        res = 0
+        while q_begin and q_end:
+            if len(q_begin) <= len(q_end):
+                res = bfs(q_begin, seen_1, seen_2)
+            else:
+                res = bfs(q_end, seen_2, seen_1)
+            if res:
+                return res
         
         return 0
