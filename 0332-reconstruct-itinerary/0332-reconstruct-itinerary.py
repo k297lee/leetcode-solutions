8d40a42ba9b39ca37f64b7712df6d90c448c2662
Kyle Yuan Yang Lee <56005940+k297lee@users.noreply.github.com>
2022-10-18 18:34:45 -0400

    Time: 226 ms (14.21%), Space: 15.3 MB (7.12%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        self.res = []
+        def backtrack(source, currPath):
+            if len(currPath) == len(tickets) + 1:
+                self.res = currPath
+                return True
+            
+            for i, nextDest in enumerate(graph[source]):
+                if not seen[source][i]:
+                    seen[source][i] = True
+                    subRes = backtrack(nextDest, currPath + [nextDest])
+                    seen[source][i] = False
+                    if subRes:
+                        return True
+            return False
+        
+        graph = defaultdict(list)
+        for ticket in tickets:
+            graph[ticket[0]].append(ticket[1])
+        
+        seen = {}
+        
+        for source, dests in graph.items():
+            dests.sort()
+            seen[source] = [False] * len(dests)
+        
+        path = ["JFK"]
+        backtrack("JFK", path)
+        return self.res
