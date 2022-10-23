0393316cf14e933b593dd559fcaaf93d13daa4bc
Kyle Yuan Yang Lee <56005940+k297lee@users.noreply.github.com>
2022-10-22 21:06:19 -0400

    Time: 961 ms (83.33%), Space: 14 MB (84.99%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -4,15 +4,12 @@ class Solution:
             return False
         
         subTotal = sum(nums) // 2
-        dp = [[False] * (subTotal + 1) for _ in range(len(nums) + 1)]
-        dp[0][0] = True
+        dp = [False] * (subTotal + 1)
+        dp[0] = True
         
-        for i in range(1, len(nums) + 1):
-            for j in range(subTotal + 1):
-                if j < nums[i - 1]:
-                    dp[i][j] = dp[i - 1][j]
-                else:
-                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - nums[i - 1]]
+        for num in nums:
+            for j in range(subTotal, num - 1, -1):
+                dp[j] = dp[j] or dp[j - num]
         
-        return dp[-1][subTotal]
+        return dp[subTotal]
         
