0b17db8b768a8b9f68b8733b15ad099428781aa9
Kyle Yuan Yang Lee <56005940+k297lee@users.noreply.github.com>
2022-10-30 23:56:21 -0400

    Time: 104 ms (29.89%), Space: 14 MB (91.00%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -8,16 +8,18 @@ class Solution:
         elif len(s2) == 0:
             return s1 == s3
         
-        dp = [[False] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+        prev = [False] * (len(s2) + 1)
         
         for i in range(len(s1) + 1):
+            curr = [False] * (len(s2) + 1)
             for j in range(len(s2) + 1):
                 if i == 0 and j == 0:
-                    dp[i][j] = True
+                    curr[j] = True
                 elif i == 0:
-                    dp[i][j] = dp[i][j - 1] and s2[j - 1] == s3[i + j - 1]
+                    curr[j] = curr[j - 1] and s2[j - 1] == s3[i + j - 1]
                 elif j == 0:
-                    dp[i][j] = dp[i - 1][j] and s1[i - 1] == s3[i + j - 1]
+                    curr[j] = prev[j] and s1[i - 1] == s3[i + j - 1]
                 else:
-                    dp[i][j] = (dp[i][j - 1] and s2[j - 1] == s3[i + j - 1]) or (dp[i - 1][j] and s1[i - 1] == s3[i + j - 1])
-        return dp[-1][-1]
+                    curr[j] = (curr[j - 1] and s2[j - 1] == s3[i + j - 1]) or (prev[j] and s1[i - 1] == s3[i + j - 1])
+            prev = curr
+        return prev[-1]
