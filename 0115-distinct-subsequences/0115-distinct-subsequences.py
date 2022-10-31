1745f64814b6c062564c5c61a2251b02bac76254
Kyle Yuan Yang Lee <56005940+k297lee@users.noreply.github.com>
2022-10-31 02:41:15 -0400

    Time: 385 ms (90.16%), Space: 14.2 MB (91.58%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,7 +1,5 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        dp = [[0] * (len(t) + 1) for _ in range(len(s) + 1)]
-        
         prev = [0] * (len(t) + 1)
         prev[len(t)] = 1
         
