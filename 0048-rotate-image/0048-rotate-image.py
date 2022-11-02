6fd630e1150d147b10cf4c64cc82fc64bf34beb0
Kyle Yuan Yang Lee <56005940+k297lee@users.noreply.github.com>
2022-11-02 00:06:36 -0400

    Time: 63 ms (62.02%), Space: 13.9 MB (75.18%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        shells = n // 2
+        
+        for shell in range(shells):
+            for i in range(shell, n - shell - 1):
+                matrix[n - 1 - i][shell], matrix[i][n - shell - 1], matrix[n - shell - 1][n - 1 - i], matrix[shell][i] = \
+                matrix[n - shell - 1][n - 1 - i], matrix[shell][i], matrix[i][n - shell - 1], matrix[n - 1 - i][shell]
+                #print(matrix)
