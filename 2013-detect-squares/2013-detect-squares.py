0d6c2520f5a4d74b8bf1a14da022df07efcc6999
Kyle Yuan Yang Lee <56005940+k297lee@users.noreply.github.com>
2022-11-02 16:35:55 -0400

    Time: 586 ms (75.76%), Space: 15.9 MB (88.46%) - LeetHub

---
+++ 2013-detect-squares/2013-detect-squares.py
@@ -0,0 +1,52 @@
+class DetectSquares:
+
+    def __init__(self):
+        self.y_map = defaultdict(set)
+        self.freqs = defaultdict(int)
+
+    def add(self, point: List[int]) -> None:
+        self.y_map[point[1]].add(point[0])
+        self.freqs[(point[0], point[1])] += 1
+
+    def count(self, point: List[int]) -> int:
+        '''
+        for p in self.freq.keys():
+            if p[1] != point[1]:
+                continue
+            
+            xs = []
+            for k in self.freq.keys():
+                if k == p or k == point[1]:
+                    continue
+                
+                if k[0] == p[1]
+        '''
+        if point[1] not in self.y_map:
+            return 0
+        
+        x_vals = self.y_map[point[1]]
+        res = 0
+        for x in x_vals:
+            if x == point[0]:
+                continue
+            d = abs(point[0] - x)
+            new_y = point[1] + d
+            
+            if new_y in self.y_map and x in self.y_map[new_y] and point[0] in self.y_map[new_y]:
+                res += self.freqs[(point[0], new_y)] * self.freqs[(x, new_y)] * self.freqs[(x, point[1])]
+            
+            new_y = point[1] - d
+            if new_y < 0:
+                continue
+            
+            if new_y in self.y_map and x in self.y_map[new_y] and point[0] in self.y_map[new_y]:
+                res += self.freqs[(point[0], new_y)] * self.freqs[(x, new_y)] * self.freqs[(x, point[1])]
+        
+        return res
+                
+
+
+# Your DetectSquares object will be instantiated and called as such:
+# obj = DetectSquares()
+# obj.add(point)
+# param_2 = obj.count(point)
