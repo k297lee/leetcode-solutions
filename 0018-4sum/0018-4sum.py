3683a412961d33a7ce68a769104d6046d0f0ec89
Kyle Yuan Yang Lee <56005940+k297lee@users.noreply.github.com>
2022-11-08 21:39:42 -0500

    Time: 998 ms (73.75%), Space: 13.9 MB (63.10%) - LeetHub

---
+++ 0018-4sum/0018-4sum.py
@@ -0,0 +1,34 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        res = set()
+        for i in range(len(nums)):
+            if nums[i] > target / 4:
+                break
+            if i == 0 or nums[i] != nums[i - 1]:
+                self.threeSum(nums, i, target - nums[i], res)
+        
+        return list(res)
+    
+    def threeSum(self, nums, i, target, res):
+        for j in range(i + 1, len(nums)):
+            if nums[j] > target / 3:
+                break
+            if j == i + 1 or nums[j] != nums[j - 1]:
+                self.twoSum(nums, i, j, target - nums[j], res)
+        
+    def twoSum(self, nums, i, j, target, res):
+        l, r = j + 1, len(nums) - 1
+        while l < r:
+            s = nums[l] + nums[r]
+            
+            if s < target:
+                l += 1
+            elif s > target:
+                r -= 1
+            else:
+                res.add((nums[i], nums[j], nums[l], nums[r]))
+                l += 1
+                r -= 1
+                #while l < r and nums[l] == nums[l + 1]:
+                #    l += 1
